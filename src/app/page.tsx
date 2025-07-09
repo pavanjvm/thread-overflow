ce5be8b187e711661b1573000d3250a5ca90f9f9
pavan prasad <pavanprasad0605@gmail.com>
2025-07-09 19:31:33 +0000

    this is too thin make it a bit thicker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,7 +64,7 @@ export default function LandingPage() {
             stroke={false}
             scale={false}
             defaultWidth={20}
-            defaultWeight={100}
+            defaultWeight={300}
           />
         </div>
         <p className="mt-4 text-xl text-muted-foreground max-w-3xl">
